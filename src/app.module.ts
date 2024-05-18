a1a44b18c0794072fe3f651b859eaddf832ba06a
nguyenminhhung <hungnm.17k2@gmail.com>
2024-05-19 00:03:34 +0700

    connect db mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './modules/products/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'hethongbanhang',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+  }
+}
