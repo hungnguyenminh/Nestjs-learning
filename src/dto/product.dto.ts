cdd9fbbc4969a316d53f1771889b4c27beb92583
nguyenminhhung <hungnm.17k2@gmail.com>
2024-05-18 23:32:54 +0700

    validation using class-validator

--- src/dto/product.dto.ts
+++ src/dto/product.dto.ts
@@ -1,5 +1,12 @@
+import { IsNotEmpty, IsNumber, MinLength } from 'class-validator';
+
 export class ProductDto {
+  @IsNotEmpty()
   categoryId?: number;
+
+  @MinLength(5, {message: 'Tên sản phẩm ít nhất 5 kí tự'})
   productName?: string;
+
+  @IsNumber()
   price?: number
 }
